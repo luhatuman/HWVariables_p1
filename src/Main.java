f94baf86c37b6d9045038d094d4cca1299b903e5
itsOtto <luhatuman@yandex.ru>
2022-11-10 00:31:06 +0900

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+    //Task 1
         var dog = 8;
         var cat = 3.6;
         var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+    //Task 2
+        dog += 4;
+        cat += 4;
+        paper += 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
