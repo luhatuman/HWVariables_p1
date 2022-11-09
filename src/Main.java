72eb652cba7a178ce4b8a00de2fc73946313251b
itsOtto <luhatuman@yandex.ru>
2022-11-10 01:11:55 +0900

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 public class Main {
     public static void main(String[] args) {
     //Task 1
-        var dog = 8;
+        System.out.println("Исходные значения");
+        float dog = 8f; // мняем тип на float
         var cat = 3.6;
         var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-
+        System.out.println("dog = " +dog);
+        System.out.println("cat = " +cat);
+        System.out.println("paper = " +paper);
     //Task 2
+        System.out.println();
+        System.out.println("Увеличиваем все значения на 4");
         dog += 4;
         cat += 4;
         paper += 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    //Task 3
+        System.out.println();
+        System.out.println("Уменьшаем dog на 3.5, cat на 1.6, paper на 7639");
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
