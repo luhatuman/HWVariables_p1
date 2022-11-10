dd9784dee798e536af36f9df772a060d5e9ee55d
itsOtto <luhatuman@yandex.ru>
2022-11-10 21:22:17 +0900

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,29 @@ public class Main {
         float dog = 8f; // мняем тип на float
         var cat = 3.6;
         var paper = 763789;
-        System.out.println("dog = " +dog);
-        System.out.println("cat = " +cat);
-        System.out.println("paper = " +paper);
-    //Task 2
+        System.out.println("dog = " + dog + "\n" + "cat = " + cat + "\n" + "paper = " + paper);
+            //Task 2
         System.out.println();
         System.out.println("Увеличиваем все значения на 4");
         dog += 4;
         cat += 4;
         paper += 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-    //Task 3
+        System.out.println(dog + "\n" + cat + "\n" + paper);
+            //Task 3
         System.out.println();
         System.out.println("Уменьшаем dog на 3.5, cat на 1.6, paper на 7639");
         dog -= 3.5;
         cat -= 1.6;
         paper -= 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        System.out.println(dog + "\n" + cat + "\n" + paper);
+        // Task 4
+        System.out.println();
+        System.out.println("(friend +2) / 7");
+            var friend = 19;
+        System.out.println(friend);
+            friend += 2;
+        System.out.println(friend);
+            friend /=7;
+        System.out.println(friend);
     }
 }
